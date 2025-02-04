e3d00531765f8443c3d2445c407a53b15b62d540
Muturi-002 <martinmnjoroge03@gmail.com>
2025-02-04 16:10:00 +0300

    Used a struct to provide output in order. Using a non-struct will pprovide an output, just not in order

--- num-api.go
+++ num-api.go
@@ -1,72 +1,89 @@
 package main
+
 import (
 	"fmt"
-	/* "errors"
-	"net/http"
-	"encoding/json" */
-	"strconv"
 	"math"
+	"net/http"
+	"strconv"
+	"encoding/json"
 	_ "github.com/gin-gonic/gin"
 )
-var prime, perfect bool
+type Number struct{
+	Num int `json:"number"`
+	Prime bool `json:"is_prime"`
+	Perfect bool `json:"is_perfect"`
+	Properties []string `json:"properties"`
+	DigitSum int `json:"digit_sum"`
+	API_Fact string `json:"fun_fact"`
+}
+var prime, perfect, err bool
 var num, sum, even, odd int
 var properties []string
 var armstrong string
 
-func Armstrong(number int) string{
-	digits:= strconv.Itoa(number)
-	digLength:= len(digits)
-	for _, digit:= range digits{
-		digitInt, _:= strconv.Atoi(string(digit))
-		sum+= int(math.Pow(float64(digitInt), float64(digLength)))
+func api(number int) {
+	// Use API to get a fun facts of a number- http://numbersapi.com/#42
+	url := fmt.Sprintf("http://numbersapi.com/%d/math", number)
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching data:",err)
+		return
 	}
-	if sum==number{
-		armstrong= "armstrong"
+	defer response.Body.Close()
+	
+}
+func Armstrong(number int) string {
+	digits := strconv.Itoa(number)
+	digLength := len(digits)
+	for _, digit := range digits {
+		digitInt, _ := strconv.Atoi(string(digit))
+		sum += int(math.Pow(float64(digitInt), float64(digLength)))
+	}
+	if sum == number {
+		armstrong = "armstrong"
 	}
 	return armstrong
 }
-func Prime(number int) bool{
-	if number <=1{
-		prime=false
+func Prime(number int) bool {
+	if number <= 1 {
+		prime = false
 	}
-	if number==2{
-		prime=true
+	if number == 2 {
+		prime = true
 	}
-	if number%2==0{
-		prime=false
+	if number%2 == 0 {
+		prime = false
 	}
-	for i:=3; i*1<=number;i+=2{
-		if number%i==0{
-			prime=false
+	for i := 3; i*1 <= number; i += 2 {
+		if number%i == 0 {
+			prime = false
 		}
 	}
-	fmt.Print("Prime?: ")
 	return prime
 }
-func Perfect(number int) bool{
-	if number<=1{
+func Perfect(number int) bool {
+	if number <= 1 {
 		return false
 	}
-	sum:=0
-	for i:=1; i<=number/2;i++{
-		if number%i==0{
-			sum+=i
+	sum := 0
+	for i := 1; i <= number/2; i++ {
+		if number%i == 0 {
+			sum += i
 		}
 	}
-	if sum==number{
-		perfect=true
-	}else{
-		perfect=false
+	if sum == number {
+		perfect = true
+	} else {
+		perfect = false
 	}
-	fmt.Print("Perfect?: ")
 	return perfect
 }
-func SumDigits(number int) int{
-	sum=0
-	digits:= strconv.Itoa(number)
-	for _, digit:= range digits{
-		digitInt, _:= strconv.Atoi(string(digit))
-		sum+=digitInt
+func SumDigits(number int) int {
+	sum = 0
+	digits := strconv.Itoa(number)
+	for _, digit := range digits {
+		digitInt, _ := strconv.Atoi(string(digit))
+		sum += digitInt
 	}
 	return sum
 }
@@ -79,34 +96,40 @@ func EvenOdd(number int) string {
 		return "Odd"
 	}
 }
-func Properties(number int) []string{
-	if Armstrong(number)=="armstrong"{
-		properties= append(properties, armstrong)
+func Properties(number int) []string {
+	if Armstrong(number) == "armstrong" {
+		properties = append(properties, armstrong)
 	}
-	if EvenOdd(number)=="Even"{
-		properties= append(properties, "Even")
-	}else{
-		properties= append(properties, "Odd")
+	if EvenOdd(number) == "Even" {
+		properties = append(properties, "Even")
+	} else {
+		properties = append(properties, "Odd")
 	}
 	return properties
 }
-func Xtics (number int){//method calling all declared methods
-	prime:=Prime(number)
-	perfect:=Perfect(number)
-	prop:=Properties(number)
-	sum:=SumDigits(number)
-	fmt.Print(prime,"\n",perfect,"\n",prop,"\n",sum)
-}
-func api(number int) {
-	// Use API to get a fun facts of a number- http://numbersapi.com/#42
-
-}
 
 func main() {
-	var num int
+	var num Number
 	fmt.Println("Enter a number: ")
-	fmt.Scan(&num)
-	fmt.Println("The number is:", num,".Check Properties---")
-	Xtics(num)
-	fmt.Println()
+	fmt.Scan(&num.Num)
+	if num.Num <=0 {
+		fmt.Println("Please enter a positive number")
+		return
+	}else{
+		num.Prime = Prime(num.Num)
+		num.Perfect= Perfect(num.Num)
+		num.DigitSum = SumDigits(num.Num)
+		num.Properties = Properties(num.Num)
+		response := map[string]interface{}{
+			"number": num.Num,
+			"is_prime": num.Prime,
+			"is_perfect": num.Perfect,
+			"properties": num.Properties,
+			"digit_sum":num.DigitSum,
+		}
+		jsonResponse, _ := json.MarshalIndent(response,"","\t")
+		json.Unmarshal(jsonResponse, &response)
+		fmt.Println(string(jsonResponse))
+	}
+	
 }
